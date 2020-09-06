c368eac8620f1c11d9c15dcac80eb6a7a97b8795
truong <nhom1hd1@gmail.com>
2020-09-06 15:50:24 +0700

    ok

--- src/containers/TheContent.js
+++ src/containers/TheContent.js
@@ -49,6 +49,12 @@ const PrivateRoute = ({
   );
 };
 
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+);
+
 const TheContent = (props) => {
   const { IsLogged, setIsLogged } = props;
 
