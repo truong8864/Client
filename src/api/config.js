2ccb6645881666092d5d2adb34ab34e486ed5969
truong <nhom1hd1@gmail.com>
2020-09-06 22:33:06 +0700

    ok

--- src/api/config.js
+++ src/api/config.js
@@ -2,14 +2,14 @@
 // export const REACT_URL_AUTHENTICATION = "http://localhost:3001/authentication";
 // export const REACT_URL_AUTHORIZATION = "http://localhost:3001/authorization";
 
-export const REACT_URL_API = "https://api-hr-manager.herokuapp.com/hrm/api/v1";
-export const REACT_URL_AUTHENTICATION =
-  "https://api-hr-manager.herokuapp.com/authentication";
-export const REACT_URL_AUTHORIZATION =
-  "https://api-hr-manager.herokuapp.com/authorization";
-
-// export const REACT_URL_API = "https://web-sv1.herokuapp.com/hrm/api/v1";
+// export const REACT_URL_API = "https://api-hr-manager.herokuapp.com/hrm/api/v1";
 // export const REACT_URL_AUTHENTICATION =
-//   "https://web-sv1.herokuapp.com/authentication";
+//   "https://api-hr-manager.herokuapp.com/authentication";
 // export const REACT_URL_AUTHORIZATION =
-//   "https://web-sv1.herokuapp.com/authorization";
+//   "https://api-hr-manager.herokuapp.com/authorization";
+
+export const REACT_URL_API = "https://web-sv1.herokuapp.com/hrm/api/v1";
+export const REACT_URL_AUTHENTICATION =
+  "https://web-sv1.herokuapp.com/authentication";
+export const REACT_URL_AUTHORIZATION =
+  "https://web-sv1.herokuapp.com/authorization";
