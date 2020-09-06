f449ecc2d50cf89ff8eed732cd4fc5f0cf381942
truong <nhom1hd1@gmail.com>
2020-09-06 16:01:00 +0700

    ok

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,9 @@
-export const REACT_URL_API = "http://localhost:3001/hrm/api/v1";
-export const REACT_URL_AUTHENTICATION = "http://localhost:3001/authentication";
-export const REACT_URL_AUTHORIZATION = "http://localhost:3001/authorization";
+// export const REACT_URL_API = "http://localhost:3001/hrm/api/v1";
+// export const REACT_URL_AUTHENTICATION = "http://localhost:3001/authentication";
+// export const REACT_URL_AUTHORIZATION = "http://localhost:3001/authorization";
 
-// export const REACT_URL_API = "https://api-hr-manager.herokuapp.com/hrm/api/v1";
-// export const REACT_URL_AUTHENTICATION = "https://api-hr-manager.herokuapp.com/authentication";
-// export const REACT_URL_AUTHORIZATION = "https://api-hr-manager.herokuapp.com/authorization";
+export const REACT_URL_API = "https://api-hr-manager.herokuapp.com/hrm/api/v1";
+export const REACT_URL_AUTHENTICATION =
+  "https://api-hr-manager.herokuapp.com/authentication";
+export const REACT_URL_AUTHORIZATION =
+  "https://api-hr-manager.herokuapp.com/authorization";
